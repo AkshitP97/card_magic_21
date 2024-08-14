cc9f923e055ef86172a53e285cbbeb1a40842e64
Akshit Patidar <apatidar32@gmail.com>
2024-08-14 23:02:41 +0530

    removed some unneccessary files

--- test.py
+++
@@ -1,9 +0,0 @@
-def arrange_in_columns(cards):
-    columns = [[], [], []]
-    for i, card in enumerate(cards):
-        columns[i % 3].append(card)
-    return columns
-
-cards = [1,2,3,4,5,6]
-
-arrange_in_columns(cards)
